a3efd7de9eefc53d462518889f55391da8cb59b4
battle_angel <six@angel>
2023-05-15 23:46:10 -0400

    math update

--- cal/luna.math.cs
+++ cal/luna.math.cs
@@ -18,19 +18,22 @@ namespace luna {
             public static float pi  = (float) Math.PI;
             public static float len = (float) 360.0;   
  
-            public delegate double   _cos ( double c );
+            public delegate double  _cos ( double c );
             public delegate double _acos ( double c );
-            public delegate double   _sin ( double c );
+            public delegate double  _sin ( double c );
+            public delegate double _asin ( double c ); 
             public delegate double _atan ( double c ); 
 
-            public static _cos     cos  = new  _cos    ( Math.Cos );
-            public static _acos  acos = new  _acos   ( Math.Acos );
-            public static _sin      sin = new  _sin     ( Math.Sin );
-            public static _atan  atan  = new _atan  ( Math.Atan); 
+            public static _cos      cos  = new  _cos  ( Math.Cos  );
+            public static _acos     acos = new  _acos ( Math.Acos );
+            public static _sin       sin = new  _sin  ( Math.Sin  );
+            public static _asin     asin = new _asin  ( Math.Asin ); 
+            public static _atan    atan  = new _atan  ( Math.Atan ); 
 
             public static float flip ( float deg )  {
                return (trig.wpi - (deg * trig.d2r)) * trig.r2d;
             }
+
             public static float quaternion_offset ( float euler_angle  ) 
             {
               float r = euler_angle * d2r; 
@@ -39,14 +42,14 @@ namespace luna {
                  return (hpi - r) * r2d; 
               else if ( r >= hpi ) 
                  return ( (450.0f * d2r) - r ) * r2d; 
+             
               else return 0.0f; 
             }
         }
 
         public static class nota { 
             public static float ten3   = (float) Math.Pow(10, 3); 
-            public static float nten3 = (float) Math.Pow(10,-3);
-             
+            public static float nten3 = (float) Math.Pow(10,-3);      
         }
 
         public static class vector {
@@ -154,7 +157,6 @@ namespace luna {
                 return ret; 
             }
 
-
             public static Vector2[] to_vector2_array ( _vector[] _arr ) {
                 Vector2[] r = new Vector2[_arr.Length];
                 for(int i = 0; i < _arr.Length; i++ ) { r[i] = to_vector2( _arr[i ] ); } 
@@ -166,14 +168,26 @@ namespace luna {
                 v.x = x; v.y = y; v.z = z; 
             }
 
-            public static _vector[ ] get_radius_vector ( _vector center, float radius  ) {
+            public static _vector[ ] get_radius_vector ( _vector center, float radius, string ori  ) {
               _vector [ ] v = new _vector[(int)trig.len];
               double cur = 0.0;
 
               for(int i = 0; i < (int) trig.len; i++ ) {
-                  v[i].x = radius * (float) cos( cur+= (double) trig.crs) + center.x;
-                  v[i].y = radius * (float) sin ( cur+= (double) trig.crs) + center.y;
-                  v[i].z = 0.0f; 
+                  switch (ori) {
+                    case "horizontal":
+                      v[i].x = radius * (float) cos ( cur+= (double) trig.crs) + center.x;
+                      v[i].y = radius * (float) sin ( cur+= (double) trig.crs) + center.y;
+                      v[i].z = center.z;  
+                      break;
+                    case "vertical":
+                      v[i].x = radius * (float) cos ( cur+= (double) trig.crs) + center.x;
+                      v[i].y = center.y;
+                      v[i].z = radius * (float) sin ( cur+= (double) trig.crs) + center.z;
+                      break;
+                    default:
+                      luna.debug.log(luna.sysdel.getmethod() + " invalid selection."); 
+                      return v; 
+                  } 
               }
               return v; 
             }
@@ -192,10 +206,19 @@ namespace luna {
                      *      ┊ 0   cos θ  -sin θ ┊
                      *      ┊ 0   sin θ   cos θ ┊
                      */
-                    r[0].x = 1.0f;  r[1].x = 0.0f;              r[2].x = 0.0f; 
-                    r[1].y = 0.0f;  r[1].y = (float) cos(t); r[2].y = (float) sin (t) * -1.0f; 
-                    r[2].z = 0.0f;  r[2].z = (float) sin(t);  r[2].z = (float) cos (t); 
+                    r[0].x = 1.0f;  r[1].x = 0.0f;            r[2].x = 0.0f; 
+                    r[0].y = 0.0f;  r[1].y = (float) cos(t);  r[2].y = (float) sin (t) * -1.0f; 
+                    r[0].z = 0.0f;  r[1].z = (float) sin(t);  r[2].z = (float) cos (t); 
                     break;
+                  case 'z': 
+                    /* Rz = |  cos θ  sin θ   0 |
+                     *      | -sin θ  cos θ   0 |
+                     *      |      0      0   1 |
+                     */
+                     r[0].x = (float) cos(t);         r[1].x = (float) sin(t); r[2].x = 0.0f; 
+                     r[0].y = (float) sin(t) * -1.0f; r[1].y = (float) cos(t); r[2].y = 0.0f;
+                     r[0].z = 0.0f;                   r[1].z = 0.0f;           r[2].z = 1.0f;
+                     break;
                 }
 
                 /** multiply */
@@ -217,31 +240,97 @@ namespace luna {
                 return r3; 
             }
 
+            // vector magnitude 
+            public static float magnitude( _vector v ) 
+            {
+               return 
+               ( 
+                  (float) num.sqrt 
+                  (
+                    num.pow ( v.x, 2.0f ) +
+                    num.pow ( v.y, 2.0f ) +
+                    num.pow ( v.z, 2.0f )
+                  )
+               ); 
+            }
         }
 
         // polar to plane converter 
         public static class plane { 
-            
             public static _vector polar_to_cart_coord ( float _long, float _lat, _plane_option _opt ) 
             {
                 return new _vector 
                 ( 
                     _opt.convert()*  zeroed( trig.pi, _long),
-                    _opt.convert()*  zeroed( trig.pi, _lat),
+                    _opt.convert()*  (_lat * trig.d2r), 
                     0.0f
                 ); 
             }
 
           public static float zeroed ( float _max_rad,  float _degree ) 
-           {
-                float rad = _degree * trig.d2r; 
-                float pos = _max_rad;
-                float neg = _max_rad * -1.0f; 
+          {
+             float rad = _degree * trig.d2r; 
+             float pos = _max_rad;
+             float neg = _max_rad * -1.0f; 
 
-                if (rad > pos) return neg - (pos - rad);
-                if (rad < neg) return pos - (neg - rad);
-                return rad; 
-           }
+             if (rad > pos) return neg - (pos - rad);
+             if (rad < neg) return pos - (neg - rad);
+             return rad; 
+          }
+
+          public static float zeroed_unity ( float _max_rad, float _angle ) 
+          {
+              return _angle / _max_rad; 
+          }
+
+
+          public static _plane_option init_plane_option 
+          (
+            float amplitude, 
+            float wave_number
+          ) 
+          {
+           return new _plane_option ( amplitude, wave_number );  
+          }
+  
+          // obtains right ascension and declination from vector 
+          public static (float ra, float dec) calculate_ra_and_dec ( _vector v ) 
+          {
+              float _ra = 0.0f; float _dec = 0.0f; 
+    
+              // first, we must obtain the direction of cosines 
+              _vector dcos = calculate_direction_of_cosines ( v ); 
+            
+              // second, we obtain declination (longitude)
+              _dec = (float) trig.asin( (double) dcos.z ); 
+
+              // third, we calculate right ascension '_ra'
+              float l = dcos.x / (float) trig.cos ( (double) _dec );  
+    
+              if ( dcos.y > 0.0f ) 
+              { 
+                _ra = (float) trig.acos ( l ); 
+              } 
+              else 
+              {
+                _ra = (float) trig.wpi - ( (float) trig.acos( l ) ); 
+              }
+
+              // return right ascension and declination
+              return (_ra, _dec); 
+          } 
+      
+          // return the direction of cosines as a vector  
+          public static _vector calculate_direction_of_cosines ( _vector v )
+          {
+            float mag = vector.magnitude ( v ); 
+            return new _vector 
+            (
+              v.x / mag,
+              v.y / mag,
+              v.z / mag
+            );
+          }
         }
 
         // polar to plane option
@@ -256,19 +345,55 @@ namespace luna {
            public float convert( ) {
              return this.wave_number / luna.cal.trig.wpi;   
            }
+
+           public string to_string ( )  {
+              string str = "plane_option:\n";
+              str+= String.Format("amplitude {0,20}", amplitude.ToString()); 
+              str+= String.Format("wavenumber {0,20}", wave_number.ToString()); 
+              return str; 
+           }
         }
 
         // <optimized out> - clamp - 2023-01-04 20:52:22
         public static class num {
-            
-            public delegate double _pow ( double x, double y ); 
-            public static Action<double, double> pow = delegate(double x, double y) { Math.Pow(x, y); };
 
+            public static float sqrt ( float v ) 
+            {
+              return (float) Math.Sqrt ( (double) v ); 
+            }
+
+            public static float pow ( float x, float y )
+            {
+              return (float) Math.Pow( (double) x, (double) y ); 
+            }
+  
             public static float clamp ( float val, float min, float max ) {
                 if (val < min) return min; else if (val > max) return max; else return val; 
             }
             
             public static float negate( float val ) { return val * -1.0f; }
+
+            public static float approach( float current, float dest, float step ) {
+                if (dest > current) 
+                  return (current + step);
+                else if ( dest < current ) 
+                  return (current - step); 
+                else 
+                  return current; 
+            }
+
+            public static bool is_modded (int i, int [ ] mods )  
+            {
+              for(int x= 0; x<mods.Length; x++) {
+                if(i%mods[x]==0) return true;
+              }
+              return false; 
+            }
+
+            public static float get_ratio ( float num, float den ) 
+            {
+              return num / den; 
+            }
         }
 
         // ref.six - vector - 2022-12-16 08:54:02 
@@ -281,6 +406,26 @@ namespace luna {
                 this.x = x; 
                 this.y = y;
                 this.z = z; 
+            }
+
+            public void translate( char axis, float n ) {
+                switch(axis) {
+                  case 'x':
+                    this.x += n;
+                    break;
+                  case 'y':
+                    this.y += n;
+                    break;
+                  case 'z':
+                    this.z += n; 
+                    break; 
+                }
+            }
+
+            public void translate( _vector target, float step ) {
+               this.x = num.approach ( this.x, target.x, step );
+               this.y = num.approach ( this.y, target.y, step );
+               this.z = num.approach ( this.z, target.z, step ); 
             }
 
             public string to_string( ) {
