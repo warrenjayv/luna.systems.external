7e67c8837046ac8d55a92502d83d64ffef7b5718
six@dakota <hannadi@mail.com>
2024-06-28 08:43:02 -0400

    fcs - tostring

--- six/fcs/target.cpp
+++ six/fcs/target.cpp
@@ -2,24 +2,36 @@
  * 
  */
 #include <string>
-using namespace std;
+#include <iostream>
 
 enum sexm { male, female };
 
 class dummy
 {
      public: 
-        string name;
+        std::string name;
         int    age;
         sexm   sex;
         
         dummy ( ) { }
+
+        dummy ( std::string _n, int _a, sexm _s)
+        {
+           name = _n; 
+           age  = _a;
+           sex  = _s;
+        }
+
+        void tostring ( ) {
+      
+          std::cout << "dummy generated: " + name + ", age: " + std::to_string(age) << std::endl; 
+        }
 };
 
 int main ( )
 { 
-     dummy a1 = dummy( );
-
+     dummy a1 = dummy( "alyssa", 24, female );
+     a1.tostring( );
     do
     {
         
