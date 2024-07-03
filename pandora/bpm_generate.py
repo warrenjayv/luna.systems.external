94f6dcbdcd5601063283a68dbb0ad31ae2e9b702
battle_angel <six@angel>
2024-07-03 01:31:21 -0400

    bpm generator, out file

--- pandora/bpm_generate.py
+++ pandora/bpm_generate.py
@@ -1,13 +1,20 @@
 import pdb;
 
 def main():
+  # get filename
+  name = input("enter filename for output\n")
+  
+  # open file
+  file = open(name, "a")
+
   # enter parameters
   start = int(input("enter starting address\n"),16)
   step  = int(input("enter step size\n"),       16)
   end   = int(input("enter end address\n"),     16)
 
   # entry
-  print("bpm " + "%X" % start)
+  # print("bpm " + "%X" % start)
+  file.write("bpm " + "%X" % start + "\n")
   
   # determine size
   size = int(( end - start ) / step); 
@@ -16,7 +23,8 @@ def main():
   val = start
   for x in range(size):
     val = val + step
-    print("bpm " + "%X" % val)
-
+    # print("bpm " + "%X" % val)
+    file.write("bpm " + "%X" % val + "\n")
+  
 main( )
 
