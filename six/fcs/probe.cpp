032f0bf9049e750aeeb5805b610757d38ea085c6
U-DAKOTA\six <six@dakota>
2024-07-09 23:05:16 -0400

    fcs - list modules

--- six/fcs/probe.cpp
+++ six/fcs/probe.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 #include <Windows.h>
+#include <memoryapi.h>
+#include <psapi.h>
 
 using namespace std;
 
@@ -9,15 +11,47 @@ HANDLE OpenProcessByID ( const DWORD id )
     return OpenProcess(PROCESS_ALL_ACCESS, false, id );
 }
 
+DWORD ListModules(const HANDLE hProcess)
+{
+    if( hProcess == NULL) { return NULL; }
+
+    HMODULE lpModule[1024];
+    DWORD   lpcbNeed;
+    uint32_t i = 0;
+
+
+    if (!EnumProcessModules(hProcess, lpModule, sizeof(lpModule), &lpcbNeed))
+        return (DWORD)NULL;
+    
+    uint32_t len = lpcbNeed / sizeof(HMODULE);
+
+    for(i = 0; i < len; i++)
+    {
+        if ( i >= len ) { break; }
+        TCHAR modName[MAX_PATH];
+        if(!GetModuleFileNameEx(hProcess, lpModule[i], modName, sizeof(modName)/sizeof(TCHAR)))
+        {
+            cout << "error!";
+        }
+        wcout << modName << endl;
+    }
+    
+
+}
+
 int main ( )
 {
     int pID;
     cout << "please enter process id:";
     cin >> pID;
 
-    cout << "open process initiated.";
+    cout << "open process initiated." << endl;
 
     HANDLE proc = OpenProcessByID ( (DWORD) pID );
+
+    ListModules(proc);
+
+    cin >> pID;
 
     return 0;
 }
